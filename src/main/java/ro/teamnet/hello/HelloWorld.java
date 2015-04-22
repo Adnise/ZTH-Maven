1aa9154fb4eae01dacd77a4009187387205d34f7
Adina Radu <adntza@gmail.com>
2015-04-22 10:20:14 +0300

    Create Maven Project

---
+++ src/main/java/ro/teamnet/hello/HelloWorld.java
@@ -0,0 +1,17 @@
+package ro.teamnet.hello;
+
+/**
+ * Created by Adina Radu on 4/22/2015.
+ */
+public class HelloWorld {
+
+    public void sayHello(){
+        System.out.println("Hello World!");
+    }
+
+    public String returnHelloKey(){
+        return "HelloKey";
+    }
+
+
+}
